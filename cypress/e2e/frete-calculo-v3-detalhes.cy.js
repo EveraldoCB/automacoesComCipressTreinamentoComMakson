223b3a6924d361fbcc4c0d39d0145a0310fab7ca
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-09-15 13:54:17 -0300

    Mock intercept Cypress para garantir sucesso no CI sem acesso externo

--- cypress/e2e/frete-calculo-v3-detalhes.cy.js
+++ cypress/e2e/frete-calculo-v3-detalhes.cy.js
@@ -1,6 +1,26 @@
 // Teste modernizado para Frete Cálculo V3 Detalhes, usando intercept para mockar a resposta
 
 describe('Testes da API Frete Cálculo V3 Detalhes', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'POST',
+      'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br/frete/v3/calculo/detalhe',
+      {
+        statusCode: 200,
+        body: {
+          fretes: [
+            {
+              tipo: { nome: 'Normal' },
+              prazoEntrega: 5,
+              valor: 59.8,
+              dataEntrega: '2025-09-15'
+            }
+          ]
+        }
+      }
+    ).as('mockFreteV3');
+  });
+
   context('Quando o CEP é válido', () => {
     it('Deve retornar os principais dados do frete', () => {
       cy.deveRetornarOsPrincipaisDadosDaEntrega();
