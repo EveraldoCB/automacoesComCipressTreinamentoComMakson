03995086a6174f365611765bcc3031e7b6fe1744
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-09-15 12:02:45 -0300

    Corrige baseUrl para ambiente hlg no Cypress config

--- Projeto de automacao de API com Cypress para apresentacao/cypress.config.js
+++ Projeto de automacao de API com Cypress para apresentacao/cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    baseUrl: 'https://frete-hub-plataforma-frete-sit.casasbahia.com.br',
+    baseUrl: 'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br',
     supportFile: 'cypress/support/e2e.js'
   }
 };
