72297debfce21f0e08468018117d521afa22bf0c
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-09-15 14:38:32 -0300

    Comando customizado ajustado.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,37 +1,15 @@
 Cypress.Commands.add('calculaFreteDetalhe', (body) => {
-  // Mock para CI: retorna resposta simulada sem acessar API externa
-  if (Cypress.env('CI')) {
-    return cy.wrap({
-      status: 200,
-      body: {
-        fretes: [
-          {
-            tipo: { nome: 'Normal' },
-            prazoEntrega: 5,
-            valor: 59.8,
-            dataEntrega: '2025-09-15'
-          }
-        ]
-      }
-    });
-  }
-  return cy.request({
-    method: 'POST',
-    url: 'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br/frete/v3/calculo/detalhe', // url absoluta
-    headers: { 'Content-Type': 'application/json' },
-    body,
-    failOnStatusCode: false
-  });
+  return cy.request('POST', 'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br/frete/v3/calculo/detalhe', body);
 });
 
-Cypress.Commands.add('deveRetornarOsPrincipaisDadosDaEntrega', () => {
-  cy.fixture('frete-calculo-v3-detalhes').then((massa) => {
-    cy.calculaFreteDetalhe(massa).then((response) => {
-      expect(response.status).to.equal(200);
+Cypress.Commands.add('deveRetornaroTipoDeEntregaoPrazoaDataeoValor', () => {
+  cy.fixture('frete-calculo-v3-detalhes').then(massa => {
+    cy.calculaFreteDetalhe(massa).then(response => {
+      expect(response.status).to.eq(200);
       const frete = response.body.fretes[0];
-      expect(frete.tipo.nome).to.equal('Normal');
-      expect(frete).to.have.property('prazoEntrega').that.is.a('number');
-      expect(frete).to.have.property('valor').that.is.a('number');
+      expect(frete.tipo.nome).to.eq('Normal');
+      expect(frete.prazoEntrega).to.be.a('number');
+      expect(frete.valor).to.be.a('number');
       expect(frete.dataEntrega).to.exist;
     });
   });
