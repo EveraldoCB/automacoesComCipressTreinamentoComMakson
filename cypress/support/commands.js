15f7c4df9ddd52696f1c8d795f201f1c77104a62
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-09-15 14:45:12 -0300

    Reativado mock para CI no comando customizado.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,12 @@
 Cypress.Commands.add('calculaFreteDetalhe', (body) => {
+  if (Cypress.env('CI')) {
+    return cy.wrap({
+      status: 200,
+      body: {
+        fretes: [{ tipo: { nome: 'Normal' }, prazoEntrega: 5, valor: 59.8, dataEntrega: '2025-09-15' }]
+      }
+    });
+  }
   return cy.request('POST', 'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br/frete/v3/calculo/detalhe', body);
 });
 
