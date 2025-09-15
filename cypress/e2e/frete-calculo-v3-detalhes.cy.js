e1be8424a7023bdf0b214deb6d0810ee732797dc
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-09-15 14:43:23 -0300

    Padronização completa para v3. Removidas todas as referências v2.

--- cypress/e2e/frete-calculo-v3-detalhes.cy.js
+++ cypress/e2e/frete-calculo-v3-detalhes.cy.js
@@ -1,29 +1,9 @@
 // Teste modernizado para Frete Cálculo V3 Detalhes, usando intercept para mockar a resposta
 
 describe('Testes da API Frete Cálculo V3 Detalhes', () => {
-  beforeEach(() => {
-    cy.intercept(
-      'POST',
-      'http://frete-hub-plataforma-frete-hlg.casasbahia.com.br/frete/v3/calculo/detalhe',
-      {
-        statusCode: 200,
-        body: {
-          fretes: [
-            {
-              tipo: { nome: 'Normal' },
-              prazoEntrega: 5,
-              valor: 59.8,
-              dataEntrega: '2025-09-15'
-            }
-          ]
-        }
-      }
-    ).as('mockFreteV3');
-  });
-
   context('Quando o CEP é válido', () => {
-    it('Deve retornar os principais dados do frete', () => {
-      cy.deveRetornarOsPrincipaisDadosDaEntrega();
+    it('Deve retornar o tipo de entrega, o prazo, a data e o valor', () => {
+      cy.deveRetornaroTipoDeEntregaoPrazoaDataeoValor();
     });
   });
 });
