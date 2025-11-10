f8ad312d3b95811d4bc9ba67309f40d0f9b3614e
JOSE EVERALDO DE BARROS <everaldo.barros@via.com.br>
2025-11-10 18:11:07 -0300

    fix: corrigir caminho do plugin @cypress/grep para versão 5.0.0

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   e2e: {
     supportFile: 'cypress/support/e2e.js',
     video: true,
-    screenshotOnRunFailure: true,
-    setupNodeEvents(on, config) {
+    screenshotOnRunFailure: true,    setupNodeEvents(on, config) {
       // Configura @cypress/grep para filtrar testes por tags
-      require('@cypress/grep/src/plugin')(config);
+      const { plugin: cypressGrepPlugin } = require('@cypress/grep/plugin')
+      cypressGrepPlugin(config)
       return config;
     },
   },
